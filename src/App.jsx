d45b1188652ffbc599ace43fd7538018a5f11b43
mdyasin111 <mdyasin7636@gmail.com>
2023-08-14 16:19:12 +0600

    use reducer used

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useReducer } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const initialState = 0;
+
+  const reducer = (state, action) => {
+    console.log(action);
+
+    if (action.type === "INCREMENT") {
+      return state + action.payload;
+    } else if (action.type === "DECREMENT") {
+      return state - action.payload;
+    } else {
+      return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="h-screen flex justify-center items-center">
+      <div className="w-full max-w-xl bg-gradient-to-tr to-purple-100 from-cyan-100 p-2 rounded">
+        <div className="bg-white rounded-md p-10">
+          <h1 className="text-2xl text-center mb-5">{state}</h1>
+          <div className="flex justify-center gap-5">
+            <button
+              onClick={() => dispatch({ type: "DECREMENT", payload: 3 })}
+              className="py-2 px-3 bg-purple-200 rounded-md"
+            >
+              Decrement
+            </button>
+            <button
+              onClick={() => dispatch({ type: "INCREMENT", payload: 5 })}
+              className="py-2 px-3 bg-cyan-200 rounded-md"
+            >
+              Increment
+            </button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
